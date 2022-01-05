b7b8d1c384ae6323c45867660158460d6b39df90
Oliver Nygren <oliver.nygren@icloud.com>
2022-01-05 19:13:33 +0100

    data file

---
+++ src/data/index.tsx
@@ -0,0 +1,25 @@
+import { MealPrep, Supplement } from '../utils';
+
+export const todoData = [
+	{
+		title: 'Magnesium',
+		category: {
+			name: 'Kosttillskott',
+			icon: <Supplement />,
+		},
+	},
+	{
+		title: 'Vitamin D',
+		category: {
+			name: 'Kosttillskott',
+			icon: <Supplement />,
+		},
+	},
+	{
+		title: 'Meal Preppa Kyckling',
+		category: {
+			name: 'Meal Prep',
+			icon: <MealPrep />,
+		},
+	},
+];
